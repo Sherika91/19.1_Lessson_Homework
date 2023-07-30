b020ef6e82889f12617e15f5a09fa4dea54d01cb
Luka Shermadini <Shermadinil.luka@gmail.com>
2023-07-30 22:57:52 +0400

    Description update

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 
 
-# Для начала определим настройки запуска
-hostName = "localhost"  # Адрес для доступа по сети
-serverPort = 8080  # Порт для доступа по сети
+# First, let's define the launch settings for our web server
+hostName = "localhost"  # Network access address
+serverPort = 8080  # Port for network access
 
 
 class MyServer(BaseHTTPRequestHandler):
     """
-        Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов
+        A special class that is responsible for
+        Processing incoming requests from customers
     """
 
-    def __get_html_content(self):
+    @staticmethod
+    def __get_html_content():
         path = "src/index.html"
 
         try:
@@ -24,7 +25,7 @@ class MyServer(BaseHTTPRequestHandler):
             return "404 Not Found"
 
     def do_GET(self):
-        """ Метод для обработки входящих GET-запросов """
+        """ Method for processing incoming GET-requests """
         query_components = parse_qs(urlparse(self.path).query)
         print(query_components)
         page_content = self.__get_html_content()
@@ -35,18 +36,18 @@ class MyServer(BaseHTTPRequestHandler):
 
 
 if __name__ == "__main__":
-    # Инициализация веб-сервера, который будет по заданным параметрам в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    # Initialization of a web server that will be on the network according to the specified parameters
+    # accept requests and send them for processing to the special class described above
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        # Web server start in an infinite loop listening for incoming requests
         webServer.serve_forever()
     except KeyboardInterrupt:
-        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        # The correct way to stop the server in the console is through the keyboard shortcut Ctrl + C
         pass
 
-    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    # Correctly stop the web server so that it releases the address and port on the network that it occupied
     webServer.server_close()
     print("Server stopped.")
